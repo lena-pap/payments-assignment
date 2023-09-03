d33cc1538d351d50892b57fa0ed81c83534fbab5
Lena Papagiannakou <lena.papagiannakou@skroutz.gr>
2023-09-03 14:51:20 +0300

    Introduce functionality for fetching price logs
    Fetch price logs based on package, year and (optionally) municipality.

--- app/lib/price_history.rb
+++ app/lib/price_history.rb
@@ -1,13 +1,21 @@
 # frozen_string_literal: true
 
 class PriceHistory
-  def self.call(...)
+  def self.call(package, year, **options)
+    beginning_of_year = DateTime.new(year)
+    end_of_year = beginning_of_year.end_of_year
+    all_year = beginning_of_year..end_of_year
 
-    # Feature request 2: Pricing History
-    #
-    # NOTE: Some tests for this file have already been written, but feel free to add
-    # more and change the old tests.
+    prices = package.prices.joins(:municipality).where(created_at: all_year)
+    municipality_name = 'municipalities.name'
 
-    raise NotImplementedError, "Implement this to pass the assignment"
+    if options[:municipality].present?
+      prices = prices.where(municipality: { id: options[:municipality] })
+      municipality_name = 'municipality.name'
+    end
+
+    prices.pluck(municipality_name, :to_price_cents).group_by(&:first).transform_values do |pric|
+      pric.transpose.second
+    end
   end
 end
--- spec/lib/price_history_spec.rb
+++ spec/lib/price_history_spec.rb
@@ -6,37 +6,64 @@ RSpec.describe PriceHistory do
   # These tests cover feature request 2. Feel free to add more tests or change
   # the existing ones.
 
-  xit "returns the pricing history for the provided year and package" do
+  it "returns the pricing history for the provided year and package" do
     basic = Package.create!(name: "basic")
+    stockholm = Municipality.create!(name: "Stockholm")
+    gotenborg = Municipality.create!(name: "Göteborg")
+    package_price_in_stockholm = PackageMunicipalityPrice.create!(
+      package: basic,
+      municipality: stockholm,
+      price_cents: 1235
+    )
+    package_price_in_gotenborg = PackageMunicipalityPrice.create!(
+      package: basic,
+      municipality: gotenborg,
+      price_cents: 1234
+    )
 
     travel_to Time.zone.local(2019) do
       # These should NOT be included
-      UpdatePackagePrice.call(basic, 20_00, municipality: "Stockholm")
-      UpdatePackagePrice.call(basic, 30_00, municipality: "Göteborg")
+      UpdatePackagePrice.call(basic, 20_00, municipality: stockholm.id)
+      UpdatePackagePrice.call(basic, 30_00, municipality: gotenborg.id)
     end
 
     travel_to Time.zone.local(2020) do
-      UpdatePackagePrice.call(basic, 30_00, municipality: "Stockholm")
-      UpdatePackagePrice.call(basic, 40_00, municipality: "Stockholm")
-      UpdatePackagePrice.call(basic, 100_00, municipality: "Göteborg")
+      UpdatePackagePrice.call(basic, 30_00, municipality: stockholm.id)
+      UpdatePackagePrice.call(basic, 40_00, municipality: stockholm.id)
+      UpdatePackagePrice.call(basic, 100_00, municipality: gotenborg.id)
     end
 
-    history = PriceHistory.call(package: basic, year: "2020")
+    history = PriceHistory.call(basic, 2020)
+
     expect(history).to eq(
       "Göteborg" => [100_00],
       "Stockholm" => [30_00, 40_00],
     )
   end
 
-  xit "supports filtering on municipality" do
+  it "supports filtering on municipality" do
     basic = Package.create!(name: "basic")
+    stockholm = Municipality.create!(name: "Stockholm")
+    gotenborg = Municipality.create!(name: "Göteborg")
+    package_price_in_stockholm = PackageMunicipalityPrice.create!(
+      package: basic,
+      municipality: stockholm,
+      price_cents: 1235
+    )
+    package_price_in_gotenborg = PackageMunicipalityPrice.create!(
+      package: basic,
+      municipality: gotenborg,
+      price_cents: 1234
+    )
 
     travel_to Time.zone.local(2020) do
-      UpdatePackagePrice.call(basic, 30_00, municipality: "Stockholm")
-      UpdatePackagePrice.call(basic, 40_00, municipality: "Stockholm")
-      UpdatePackagePrice.call(basic, 100_00, municipality: "Göteborg")
+      UpdatePackagePrice.call(basic, 30_00, municipality: stockholm.id)
+      UpdatePackagePrice.call(basic, 40_00, municipality: stockholm.id)
+      UpdatePackagePrice.call(basic, 100_00, municipality: gotenborg.id)
     end
 
-    # Whoops no assertions, please add some
+    history = PriceHistory.call(basic, 2020, municipality: stockholm.id)
+
+    expect(history).to eq("Stockholm" => [30_00, 40_00])
   end
 end
